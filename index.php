c3246186c571b5db23772d5481e01ff0c313c509
Daniel Corn <d@cundd.net>
2016-02-27 12:53:48 +0100

    Removed deprecated mbstring INI settings

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
 
 error_reporting(E_ALL);
 ini_set('default_charset', 'utf-8');
-ini_set('mbstring.internal_encoding', 'utf-8');
-ini_set('mbstring.http_output', 'UTF-8');
 ini_set('mbstring.encoding_translation', TRUE);
 ini_set('mbstring.func_overload', 6);
 
